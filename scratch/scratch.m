dc080be8f61551c8379b1793d9dacdd8dfc22868
Durban Keeler <u1046484@lonepeak1.wasatch.peaks>
2022-03-24 14:47:58 -0600

    Adds tmp files to be merged into stitch

---
+++ scratch/scratch.m
@@ -0,0 +1,8 @@
+% Experimental workspace
+
+% Define path to image directory and (stitched) image name
+im_dir = '../tutorial/npi/heximap/DZB1212-500105L005001_6001/';
+im_name = 'DZB1212-500105L005001';
+
+
+corners = get_corners(im_dir, im_name);
