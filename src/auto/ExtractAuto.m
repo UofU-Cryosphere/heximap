3b26693770909aca8feda48f4ab352b8ca09f641
Durban G. Keeler <durban.keeler@gmail.com>
2022-06-24 13:32:02 -0600

    Adds temporary manually-curated ROI processing window

--- src/auto/ExtractAuto.m
+++ src/auto/ExtractAuto.m
@@ -1,7 +1,14 @@
 % Function to automatically extract DEM values from a pair of Hexagon
 % images.
 
-function [objDEM] = ExtractAuto(objM1, IM1_meta, objM2, IM2_meta)
+function [objDEM] = ExtractAuto(objM1, IM1_meta, objM2, IM2_meta, ...
+    strExportPath, strRes, iBlkSz)
+
+% Define export directory (create if needed)
+strExPath = fullfile(strExportPath, "extraction/");
+if ~isfolder(strExPath)
+    mkdir(strExPath)
+end
 
 % Define rough corner positions for initial georeferencing and alignment
 EXT_FUNC.ControlPoints(objM1, IM1_meta);
@@ -19,5 +26,40 @@ cM = cM(vOrder);
 % homographies using full images. More accurate matrices are computed
 % later for each processing window.
 EXT_FUNC.InitTrans(cM);
+
+% Define regions of interest within overlap between pairs
+% This will eventually subset to a Python-generated ROI based on glacier
+% locations
+% % % cWindow = EXT_FUNC.DefineRegions(cM, rgi_dat);
+% Until that is implemented, here is a manually-curated variable for
+% development and testing (a 2x2 window region near the center)
+sWin1 = struct(...
+    'left', [32251, 9388; 37146, 14350], ...
+    'right', [13243, 9288; 18138, 14250], ...
+    'region', 1);
+sWin2 = struct(...
+    'left', [32251, 13787; 37146, 18751], ...
+    'right', [13228, 13653; 18123, 18617], ...
+    'region', 1);
+sWin3 = struct(...
+    'left', [36646, 9386; 41549, 14353], ...
+    'right', [17565, 9286; 22468, 14253], ...
+    'region', 1);
+sWin4 = struct(...
+    'left', [36647, 13784; 41549, 18753], ...
+    'right', [17551, 13655; 22453, 18624], ...
+    'region', 1);
+cWindow = {sWin1, sWin2, sWin3, sWin4};
+
+% Rectify the stereo images, compute disparity maps
+EXT_FUNC.DisparityLoop(cM,strExPath,cWindow,strRes,iBlkSz);
+
+% Refine camera orientations using bundle adjustment
+extBundleAdjustLoop(strExPath);
+
+% Triangulate the points
+extTriangulateLoop(strExPath);
+
+
 
 end
